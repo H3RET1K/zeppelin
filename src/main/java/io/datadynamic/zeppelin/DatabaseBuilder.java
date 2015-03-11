cc3a208ac23af9cbe4f754ba183271519c985258
Riley <riley.m.miller.87@gmail.com>
2015-03-10 23:15:16 -0500

    removed comments from dbbuilder

--- src/main/java/io/datadynamic/zeppelin/DatabaseBuilder.java
+++ src/main/java/io/datadynamic/zeppelin/DatabaseBuilder.java
@@ -79,31 +79,6 @@ public class DatabaseBuilder {
         } catch (Exception ex) {
             System.out.println("Cannot create database stucture.");
             System.exit(1);
-        }
-        /*
-        Class.forName("org.h2.Driver");
-        Connection setupConn = DriverManager.getConnection("jdbc:h2:mem:");
-        
-        setupConn.createStatement()
-            .executeUpdate("CREATE TABLE IF NOT EXISTS users (id identity, username varchar(140), password varchar(max), role varchar(140))");        
-        
-        ResultSet adminExists = setupConn.createStatement().executeQuery("SELECT top 1 username FROM users where username = 'admin'");
-        boolean hasAdmin = false;
-        while(adminExists.next()) {
-            hasAdmin = true;
-        }
-        adminExists.close();
-        if(!hasAdmin) {
-            String pw_hash = BCrypt.hashpw("password", BCrypt.gensalt());
-            System.out.print("admin username does not exist so creating...");
-            PreparedStatement pstmt =
-            setupConn.prepareStatement("insert into users(username, password, role) values ('admin', ?, 'admin')");
-            pstmt.setString(1, pw_hash);
-            pstmt.execute();
-            System.out.print("done!\n");
-        }        
-        
-        setupConn.close();
-        */
+        }     
     }
 }
